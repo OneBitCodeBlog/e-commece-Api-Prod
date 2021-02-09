740ce77e1c9694971be108c687bb8b81d884f5d1
Leonardo Scorza <contato@onebitcode.com>
2021-02-09 09:03:55 -0300

    ista de categorias

---
+++ app/controllers/storefront/v1/categories_controller.rb
@@ -0,0 +1,7 @@
+module Storefront::V1
+  class CategoriesController < ApplicationController
+    def index
+      @categories = Category.order(:name)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
-
-  namespace :admin do
+  
+  namespace :admin, defaults: { format: :json } do
     namespace :v1 do
       get "home" => "home#index"
       resources :categories
+      resources :coupons
+      resources :games, only: [], shallow: true do
+        resources :licenses
+      end
       resources :products
+      resources :system_requirements
+      resources :users
     end
   end
 
   namespace :storefront do
     namespace :v1 do
-      get 'home' => 'home#index'
+      get "home" => "home#index"
       resources :products, only: [:index, :show]
+      resources :categories, only: :index
     end
   end
 end
